6e627af4dd86f19fec9638a69228c715a2d80328
DISHANT1010 <144822986+DISHANT1010@users.noreply.github.com>
2024-07-10 10:43:02 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+ const account_Id = 123456
+ let account_Email = "dishant@gmail.com"
+ var account_Password = "12345";
+let account_State;
+ account_City = "surat" // not recomanded
+
+ account_Email = "d@gmail.com"
+ account_Password = "345"
+ account_City = "Ahemdabad"
+
+//  account_Id = 2
+
+// console.log(account_Id);
+
+/*
+
+*/
+
+console.table([account_Id , account_Email,account_Password,account_City,account_State])
