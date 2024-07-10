cf241cd4a5b6118f6d905ffd9235b966c8f5268a
DISHANT1010 <144822986+DISHANT1010@users.noreply.github.com>
2024-07-10 11:06:31 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all the js code as newer version
+
+// alert("Hello") we are using node js not browser
+
+console.log(2+2)
+// code redablity is very important
+console.log("Apple")
+
+let name = "Subhesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// premetive data type
+
+// Number
+// bigInt
+// String
+// boolean => true or false
+// null => standlone value
+// undefined => value is not assigned
+// Symbole => unique
+
+// object
+
+console.log(typeof null); // typeof null return the object 
+/*
+    Importane Interview 
+    The "null" is an object
+*/
+
+console.log(typeof undefined); // typeof undefined return the undefined
