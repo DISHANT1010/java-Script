eae69c9aabe86e4453d4cce3adab4936655607a3
DISHANT1010 <144822986+DISHANT1010@users.noreply.github.com>
2024-07-11 04:24:59 +0000

    comparision

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+
+// make 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); 
+// converting null in to number is treating null as 0
+// ******************* IMPORTANT **********************//
+// In the javascript the comparision and equality check is diffrent 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// Strictly check (===) it compare not only value but check value and dataType
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
